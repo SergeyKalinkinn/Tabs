90afa38d0bf5d25941a1769237c2cfc49c09f9f6
SergeyKalinkinn <109279066+SergeyKalinkinn@users.noreply.github.com>
2023-01-13 19:45:37 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+window.addEventListener('DOMContentLoaded', function () {
+
+    'use strict';
+    let tab = document.querySelectorAll('.info-header-tab'),
+        info = document.querySelector('.info-header'),
+        tabContent = document.querySelectorAll('.info-tabcontent');
+
+    // Скрываем табы
+    function hideTabContent(a) {
+        for (let i = a; i < tabContent.length; i++) {
+            tabContent[i].classList.remove('show');
+            tabContent[i].classList.add('hide');
+        }
+    }
+    // Вместо "а" ставим "1" и скрываем все пункты кроме первого
+    hideTabContent(1);
+
+    // показываем табы
+    function showContent(b) {
+        if (tabContent[b].classList.contains('hide')) {
+            tabContent[b].classList.remove('hide');
+            tabContent[b].classList.add('show');
+        }
+    }
+
+    // Назначаем обработчик событий при клике на табы
+    info.addEventListener('click', function (event) {
+        let target = event.target;
+        if (target && target.classList.contains('info-header-tab')) {
+            for (let i = 0; i < tab.length; i++) {
+                if (target == tab[i]) {
+                    hideTabContent(0);
+                    showContent(i);
+                    break;
+                }
+            }
+        }
+    })
+});
